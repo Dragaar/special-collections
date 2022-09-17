22973a9924cf78419f3fc372078668e5ebee267b
Dragaar <rosivanyshyn@gmail.com>
2022-09-17 14:43:58 +0300

    333

--- src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
+++ src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
@@ -1,9 +1,30 @@
 package com.efimchick.ifmo.collections;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
-class MedianQueue extends LinkedList {
+class MedianQueue extends LinkedList<Integer> {
 
+    @Override
+    public Integer poll() {
+        Integer toRemove = peek();
+        remove(toRemove);
+        return toRemove;
+    }
 
+    @Override
+    public Integer peek() {
+        return median();
+    }
+
+    private Integer median(){
+
+        int medianIndex = (this.size()-1)/2;
+        //сортування в нормальному порядку
+        this.sort(null);
+
+        return (Integer) this.toArray()[medianIndex];
+    }
 }
